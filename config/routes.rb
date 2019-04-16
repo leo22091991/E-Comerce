d1be3a07f71f3a0c0fc01d3e08e3783e1a6a24c1
Leonardo Drago <leo.ipt.13@gmail.com>
2019-04-16 02:20:33 -0300

    Se agrego campos a la vista con el listado de productosagregados al carrito

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :carts
   get 'welcome/index'
   root 'welcome#cart'
 
   resources :sales
   resources :categories
-  resources :products
+  resources :products do
+  	resources :categories, only: [:show]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_202141) do
+ActiveRecord::Schema.define(version: 2019_04_15_200432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.float "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
